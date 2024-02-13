6af38c080a99e6406b39a1b272721aca5aa468bc
MatheusHBMelo <matheushbmelo@gmail.com>
2024-02-13 18:40:41 -0300

    Saida de dados no javascript

---
+++ 01 - JS Output/index.js
@@ -0,0 +1,16 @@
+// A linguagem javascript pode exibir informações de diferentes maneiras, como por exemplo:
+
+// 01 - Escrevendo em um elemento HTML com InnerHTML
+document.getElementById("idInner").innerHTML = "Texto usando InnerHTML"
+
+// 02 - Escrevendo em uma saida de HTML com document.write()
+document.write("Texto usando document.write()")
+
+// 03 - Escrevendo em uma caixa de alerta com window.alert()
+window.alert("Texto usando window.alert()")
+
+// 04 - Escrevendo no console do navegador com console.log
+console.log("Texto usando console.log")
+
+// 05 - Printando a tela com window.print()
+window.print()
