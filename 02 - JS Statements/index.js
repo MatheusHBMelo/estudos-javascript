4ec0f603a23fafa49cd55b157dd02a8fba25c16e
MatheusHBMelo <matheushbmelo@gmail.com>
2024-02-13 19:10:44 -0300

    Statement no javascript

---
+++ 02 - JS Statements/index.js
@@ -0,0 +1,11 @@
+// 01 - Declarações em javascript
+// As instruções JavaScript são compostas por: Valores, operadores, expressões, palavras-chave e comentários.
+document.getElementById("demo").innerHTML = "Hello World";
+
+// 02 - Ponto e virgula
+// Ponto e vírgula separam instruções JavaScript.
+let a, b, c;  
+a = 5;        
+b = 6;        
+c = a + b;
+a = 5; b = 6; c = a + b;    
